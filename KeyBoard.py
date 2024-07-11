7a777399b4e1f4cab4bf7c415aa9037d5f6e31cb
Igreeda <82223725+Igggor@users.noreply.github.com>
2024-07-11 15:19:29 +0300

    Delete KeyBoard.py

--- KeyBoard.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo, InputMediaPhoto, InputMediaVideo
-kb = [
-        [KeyboardButton(
-            "Окрыть форму отчетов",
-            web_app=WebAppInfo("https://igggor.github.io")  # obviously, set yours here.
-        )]
-    ]
